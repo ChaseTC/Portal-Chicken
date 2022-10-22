38ad8f79d4aa18ccfe5f891c2ec8dc9433440f96
chav0-moe <61418955+chav0-moe@users.noreply.github.com>
2022-10-22 21:51:33 +0200

    Sound effects added

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private AudioSource finishSoundEffect;
+
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "player" || col.gameObject.tag == "egg")
         {
             // Finish Logic Here
             Debug.Log("Finished Level");
+            finishSoundEffect.Play();
         }
     }
 }
